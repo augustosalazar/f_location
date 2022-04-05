af61aaf5edc51234ab5b88d549a7c8d604272011
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2022-04-05 09:51:39 -0500

    update kotlin version

--- lib/domain/controller/location_controller.dart
+++ lib/domain/controller/location_controller.dart
@@ -9,7 +9,7 @@ import 'package:loggy/loggy.dart';
 //import 'package:latlong2/latlong.dart';
 
 class LocationController extends GetxController {
-  final userLocation = UserLocation(latitude: 0, longitude: 0).obs;
+  var userLocation = UserLocation(latitude: 0, longitude: 0).obs;
   var errorMsg = "".obs;
   var _liveUpdate = false.obs;
   //var markers = <Marker>[].obs;
